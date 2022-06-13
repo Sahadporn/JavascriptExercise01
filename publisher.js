52b3bc7f72fc7f952082912ad0dd8afffa194d1a
Sahadporn Charnlertlakha <59825486+Sahadporn@users.noreply.github.com>
2022-06-13 16:41:10 +0700

    second commit

--- publisher.js
+++ publisher.js
@@ -1,75 +1,73 @@
-const amqp = require("amqplib/callback_api");
-const fs = require("fs");
+const amqp = require("amqplib/callback_api")
+const fs = require("fs")
 
 function convertToJson(id, name, age) {
   var msg = {
     _id: id,
     name: name,
     age: parseInt(age),
-  };
+  }
 
-  return msg;
+  return msg
 }
 
 function readCsv(channel, exchange, file) {
-  const data = fs.readFileSync(file, "utf8");
+  const data = fs.readFileSync(file, "utf8")
 
-  var rows = data.slice(data.indexOf("\n") + 1).split("\n");
+  var rows = data.slice(data.indexOf("\n") + 1).split("\n")
 
-  rows.map(function (row) {
-    var values = row.split(",");
-    var converted = convertToJson(values[0], values[1], values[2]);
-    
-    setTimeout(() => {
-      publish(channel, exchange, converted);
-    }, 500);
-  });
+  rows.map((row) => {
+    var values = row.split(",")
+    var converted = convertToJson(values[0], values[1], values[2])
+
+    publish(channel, exchange, converted)
+  })
 }
 
 function publish(channel, exchange, msg) {
   // convert data to json string first
-  channel.publish(exchange, "", Buffer.from(JSON.stringify(msg)));
-  console.log(" [x] Sent %s", msg);
+  channel.publish(exchange, "", Buffer.from(JSON.stringify(msg)))
+  console.log(" [x] Sent %s", msg)
 }
 
-var cred = {
+const cred = {
   credentials: require("amqplib").credentials.plain("root", "root"),
-};
+}
 
-amqp.connect("amqp://localhost", cred, function (error0, connection) {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel(function (error1, channel) {
-    if (error1) {
-      throw error1;
-    }
+amqp.connect("amqp://localhost", cred, (error0, connection) => {
+  if (error0) throw error0
 
-    var exchange = "jsExchange";
+  connection.createChannel((error1, channel) => {
+    if (error1) throw error1
 
+    var exchange = "jsExchange"
+
+    //declare exchange
     channel.assertExchange(exchange, "fanout", {
       durable: true,
-    });
+    })
 
+    // declare queue
     channel.assertQueue("jsQueue", {
       durable: true,
-    });
+    })
 
-    channel.bindQueue("jsQueue", "jsExchange", "");
+    // bind queue to exchange
+    channel.bindQueue("jsQueue", "jsExchange", "")
 
-    var msg = process.argv.slice(2).join(" ");
+    var msg = process.argv.slice(2).join(" ")
+    var arr = msg.split(",")
 
     if (msg[0] == "i") {
-      var arr = msg.split(",");
-      msg = convertToJson(arr[1], arr[2], arr[3]);
-      publish(channel, exchange, msg);
+      msg = convertToJson(arr[1], arr[2], arr[3])
+      publish(channel, exchange, msg)
     } else if (msg[0] == "c") {
-      readCsv(channel, exchange, msg.substring(2));
+      readCsv(channel, exchange, arr[1])
     }
-  });
+  })
 
-  setTimeout(function () {
-    connection.close();
-    process.exit(0);
-  }, 1000);
-});
+  setTimeout(() => {
+    connection.close()
+    process.exit(0)
+  }, 1000)
+})
--- redis_consume.js
+++ redis_consume.js
@@ -1,117 +1,116 @@
-const { MongoClient } = require("mongodb");
-const redis = require("redis");
+const { MongoClient } = require("mongodb")
+const redis = require("redis")
 
-var amqp = require("amqplib/callback_api");
+const amqp = require("amqplib/callback_api")
 
-var cred = {
+const cred = {
   credentials: require("amqplib").credentials.plain("root", "root"),
-};
+}
 
-var info_col;
+var info_col
 
 // connect to mongodb
 MongoClient.connect("mongodb://root:root@localhost:27017/", (error, db) => {
-  if (error) throw error;
+  if (error) throw error
 
-  db = db.db("jsInfoRedis");
-  info_col = db.collection("info");
-});
+  db = db.db("jsInfoRedis")
+  info_col = db.collection("info")
+})
+
+const client = redis.createClient({
+    host: "localhost",
+    port: 6379,
+    password: "root",
+  })
 
 // connect to rabbitmq
-amqp.connect("amqp://localhost", cred, function (error0, connection) {
-  if (error0) throw error0;
+amqp.connect("amqp://localhost", cred, (error0, connection) => {
+  if (error0) throw error0
 
-  connection.createChannel(function (error1, channel) {
-    if (error1) throw error1;
+  connection.createChannel((error1, channel) => {
+    if (error1) throw error1
 
-    var exchange = "jsExchange";
+    var exchange = "jsExchange"
 
     // create an exchange
     channel.assertExchange(exchange, "fanout", {
       durable: true,
-    });
+    })
 
     // create a queue
-    channel.assertQueue(
+    var q = channel.assertQueue(
       "jsQueue",
       {
         durable: true,
       },
-      function (error2, q) {
-        if (error2) throw error2;
+      error2 => {
+        if (error2) throw error2
 
-        console.log(" [*] Waiting for logs. To exit press CTRL+C");
-
-        // bind queue to exchange
-        channel.bindQueue(q.queue, exchange, "");
-
-        // start consuming
-        channel.consume(
-          q.queue,
-          function (msg) {
-            var content = JSON.parse(msg.content);
-            console.log(content);
-
-            var get_redis;
-            // connect to redis
-            (async () => {
-              try {
-                const client = redis.createClient({
-                  host: "localhost",
-                  port: 6379,
-                  password: "root",
-                });
-                await client.connect();
-                get_redis = await client.get(content["_id"]);
-
-                // check with redis
-                if (get_redis == null) {
-                  var date = {
-                    created_at: new Date(),
-                    updated_at: new Date(),
-                  };
-
-                  var merged = Object.assign(content, date);
-
-                //   console.log(JSON.stringify(merged));
-
-                  client.set(merged["_id"], merged["created_at"].toString());
-                  info_col.insertOne(merged, (err) => {
-                      if (err) throw err;
-                      console.log('Save Success');
-                  });
-                } else {
-                  info_col.updateOne(
-                    { _id: content["_id"] },
-                    {
-                      $currentDate: {
-                        updated_at: true,
-                      },
-                      $set: {
-                        name: content["name"],
-                        age: parseInt(content["age"]),
-                      },
-                    },
-                    (err) => {
-                      if (err) throw err;
-                      console.log('Update Success');
-                    }
-                  );
-                }
-
-                client.quit();
-              } catch (err) {
-                console.error(err);
-              }
-            })();
-
-            channel.ack(msg);
-          },
-          {
-            noAck: false,
-          }
-        );
+        console.log(" [*] Waiting for logs. To exit press CTRL+C")        
       }
-    );
-  });
-});
+    )
+
+    // bind queue to exchange
+    channel.bindQueue(q.queue, exchange, "")
+
+    // start consuming
+    channel.consume(
+      q.queue,
+      msg => {
+        var content = JSON.parse(msg.content)
+        console.log(content)
+
+        var get_redis
+        // connect to redis
+        ;(async () => {
+          try {
+            await client.connect()
+            get_redis = await client.get(content["_id"])
+
+            // check with redis
+            if (get_redis === null) {
+              var date = {
+                created_at: new Date(),
+                updated_at: new Date(),
+              }
+
+              var merged = Object.assign(content, date)
+
+              client.set(merged["_id"], merged["created_at"].toString())
+              info_col.insertOne(merged, err => {
+                if (err) throw err
+                console.log("Save Success")
+              })
+            } else {
+              info_col.updateOne(
+                { _id: content["_id"] },
+                {
+                  $currentDate: {
+                    updated_at: true,
+                  },
+                  $set: {
+                    name: content["name"],
+                    age: parseInt(content["age"]),
+                  },
+                },
+                err => {
+                  if (err) throw err
+                  console.log("Update Success")
+                }
+              )
+            }
+
+            client.quit()
+          } catch (err) {
+            console.error(err)
+          }
+        })()
+
+        channel.ack(msg)
+      },
+      {
+        noAck: false,
+      }
+    )
+  })
+})
